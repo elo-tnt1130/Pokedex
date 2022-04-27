562da27f31cf4a693df8a2a932c5f78d7da00b0d
elo-tnt1130 <elo.caille@gmail.com>
2022-04-28 00:44:10 +0200

    Team : creation d'1 flatlist. Images et navigation non fonctionnelles

--- screens/Team.js
+++ screens/Team.js
@@ -1,16 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, FlatList, Text, View, Image } from "react-native";
-import SearchPokemon from '../components/SearchBox'
+import { StyleSheet, FlatList, View, Image } from "react-native";
+import { retrieveData } from "../utils/localStorage";
+import PokemonCard from "../components/PokemonCard";
+import { getPokemon } from "../API/PokeApi";
 
-export default function Search(props) {
+export default function Team(props) {
+  const { route, navigation, ...restProps } = props;
 
-  const {route, navigation, ...restProps } = props
+  const [team, setTeam] = useState([]);
 
-    return (
+  useEffect(() => {
+    
+    retrieveData("Team").then((res) => {
+      if (res) {
+        let datas = JSON.parse(res);
+        console.log(datas);
+        setTeam(datas);
+      }
+    });
+
+  }, []);
+
+  return (
     <>
       <View style={styles.container}>
-
+        <FlatList
+          data={team}
+          numColumns={3}
+          renderItem={({ item }) => (
+            <PokemonCard name={item.name} url={item.url} navigation={navigation}/>
+          )}
+          keyExtractor={(item) => item.name}
+          style={styles.list}
+          onEndReachedThreshold={0.5}
+        />
       </View>
       <StatusBar style="auto" />
     </>
@@ -21,5 +45,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 0,
     alignItems: "center",
-  }
+  },
 });
